e8c01acbbfa66a8cf2e2d6ce505b712d0e274418
maya <136956080+mayaheartsu@users.noreply.github.com>
2026-02-17 10:27:47 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,24 +1,22 @@
 #!/bin/sh
 
-# RUN THIS SCRIPT AS SUDO
+# DO NOT RUN THIS SCRIPT AS SUDO - WILL RESULT IN INCORRECT OWNERSHIP/PERMISSIONS
 # Maya Mac Dotfiles installer - local copy saved in icloud in addition to this git repo
-# installs homebrew, installs brewfile, copies /.config folders/items from repo as well as zsh configurations to appropriate locations
+# installs homebrew, installs brewfile, creates & copies config folders/items and zsh configuration from repo to appropriate locations
 
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# Install git via Homebrew
-brew install git
-
 # Clone git repo for access to config files
 git clone https://github.com/mayaheartsu/mac-dotfiles
 
-# Install Brewfile & cleanup residual dependancies
+# Install Brewfile
 cp ~/mac-dotfiles/Brewfile ~/
 brew bundle
 brew bundle cleanup --global --force
 
-# Copy aerospace, sketchybar, oh-my-posh, zsh, kitty, & yazi configs from local repo to .config locations
+# Create config directory, copy aerospace, sketchybar, oh-my-posh, zsh, kitty, & yazi configs from repo to .config locations
+mkdir ~/.config
 cp -r ~/mac-dotfiles/.config/* ~/.config
 cp ~/mac-dotfiles/.p10k.zsh ~/
 cp ~/mac-dotfiles/.zshrc ~/
